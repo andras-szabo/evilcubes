520e80094a3c321aa2b9b15ad5e895c1d5c5d7f8
andras <andras.szabo.fin@gmail.com>
2019-03-23 14:28:29 +0100

    WIP: cube awareness system; where cubes nearby one another try to keep track of one another, to be able to reconcile paths

---
+++ Assets/Movement/ICollidable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollidable
+{
+	bool OverlapsPosition(Vector3 position, float cubeSize);
+	bool OverlapsAnyPosition(IEnumerable<Vector3> positions, float cubeSize);
+}
---
+++ Assets/Movement/TestCollisionChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollisionChecker : MonoBehaviour 
+{
+	private List<ICollidable> otherCubesNearby;
+
+	public bool IsPathFree(IEnumerable<Vector3> path, float cubeSize)
+	{
+		foreach (var cube in otherCubesNearby)
+		{
+			if (cube.OverlapsAnyPosition(path, cubeSize))
+			{
+				return false; 
+			}
+		}
+
+		return true;
+	}
+}
