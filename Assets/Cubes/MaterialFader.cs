01ca59d4c15318985f11cfa7f1e39faf8c52965b
andras <andras.szabo.fin@gmail.com>
2019-03-24 17:02:04 +0100

    Creating transparent standard surface shader with instanced '_Color' property, to have fading cubes

---
+++ Assets/Cubes/MaterialFader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MaterialFader : MonoBehaviour 
+{
+	public HP hpToObserve;
+
+	public Color color;
+	private Renderer _renderer;
+	private MaterialPropertyBlock _materialPropertyBlock;
+
+	private void Start()
+	{
+		ObserveHP();
+		SetupRendererAndStartColor();
+	}
+
+	private void ObserveHP()
+	{
+		if (hpToObserve != null)
+		{
+			hpToObserve.OnHitPointsChanged += HandleHitPointsChanged;
+		}
+	}
+
+	private void SetupRendererAndStartColor()
+	{
+		_renderer = GetComponent<Renderer>();
+		_materialPropertyBlock = new MaterialPropertyBlock();
+
+		_renderer.GetPropertyBlock(_materialPropertyBlock);
+		_materialPropertyBlock.SetColor("_Color", color);
+		_renderer.SetPropertyBlock(_materialPropertyBlock);
+	}
+
+	private void OnDestroy()
+	{
+		if (hpToObserve != null)
+		{
+			hpToObserve.OnHitPointsChanged -= HandleHitPointsChanged;
+		}
+	}
+
+	private void HandleHitPointsChanged(HPInfo hpInfo)
+	{
+		var currentOpacity = hpInfo.RateToFull;
+
+		_renderer.GetPropertyBlock(_materialPropertyBlock);
+		_materialPropertyBlock.SetColor("_Color", new Color(color.r, color.g, color.b, currentOpacity));
+		_renderer.SetPropertyBlock(_materialPropertyBlock);
+	}
+}
