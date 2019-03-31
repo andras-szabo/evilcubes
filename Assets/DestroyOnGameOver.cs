c89afb78a8f41d850e72f4b72c280d329d0b2b8f
andras <andras.szabo.fin@gmail.com>
2019-03-31 09:59:19 +0200

    DestroyOnGameOver, and apply it to bullet trails

---
+++ Assets/DestroyOnGameOver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DestroyOnGameOver : MonoBehaviour
+{
+	private void Start()
+	{
+		var gc = ManagerLocator.TryGet<GameController>();
+		if (gc != null)
+		{
+			gc.OnGameOver += HandleGameOver;
+		}
+	}
+
+	private void HandleGameOver(bool hasPlayerWon)
+	{
+		Destroy(this.gameObject);
+	}
+
+	private void OnDestroy()
+	{
+		var gc = ManagerLocator.TryGet<GameController>();
+		if (gc != null)
+		{
+			gc.OnGameOver -= HandleGameOver;
+		}
+	}
+}
